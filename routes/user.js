135e68c2ff51737e7e3afcf486b20e78ac0f0b90
tamccullough88 <tamccullough88@gmail.com>
2024-01-10 19:47:20 -0500

    adding comments model file

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { getAllUsers, getUserById, createUser, deleteUser } = require('../controllers/user')
+const { getAllUsers, getUserById, createUser, deleteUser } = require('../controllers/user_controller')
 
 
 router.get('/all', getAllUsers)
