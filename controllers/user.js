55e6805a4bd114f796fc5deb607b04a0d6c84510
Cwdrian <xcharlesdrian@gmail.com>
2024-01-17 22:42:29 -0500

    update

--- controllers/user.js
+++ controllers/user.js
@@ -16,8 +16,8 @@ async function getAllUsers(req, res) {
 // Show user by ID
 async function getUserById(req, res) {
     try {
-        const { _id } = req.params;
-        const user = await User.findById(_id);
+        const { id } = req.params;
+        const user = await User.findById(id);
 
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
@@ -99,5 +99,4 @@ module.exports = {
     deleteUser,
     loginUser,
 };
-
 
