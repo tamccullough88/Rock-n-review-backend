d3670214b6a9e12a80497edf0050917713b7f032
tamccullough88 <tamccullough88@gmail.com>
2024-01-20 19:59:43 -0500

    updating routes in reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { getAllReviews, submitReview, getAlbumReview, deleteReview, updateReview } = require('../controllers/reviews');
 
+
 router.get('/all', getAllReviews);
 router.post('/', submitReview);
 router.get('/:albumId', getAlbumReview);
-router.delete('/:id', deleteReview);
 router.put('/:id', updateReview);
+router.delete('/:id', deleteReview);
 
 module.exports = router;
